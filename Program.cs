9fb11e35d8c48e46d69f5d72a8aa86e263ada166
Ekaterina Greben <katenka26@gmail.com>
2022-09-18 15:08:38 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Hello, World! Version 0.0.1");
+
+string filename = "/home/user/Projects/CSVReader/employee.csv";
+
+
+using (StreamReader reader = new StreamReader(filename))
+{
+    string? line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        System.Console.WriteLine(line);
+    }
+}
